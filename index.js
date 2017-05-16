cf7de53b3b18ec3be890bdef5e2b4a4a8a525f93
Simon Emanuel Schmid <simon@schmid.io>
2017-05-16 18:38:15 +0200

    build the chain. takes too long

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const BigNumber = require('bignumber.js');
 // check if the md5 function corresponds to the given rainbow-table chain
 assert(md5('0000000') === '29c3eea3f305d6b823f562ac4be35217');
 
-const initialPassword = '0000000';
 const passwordLength = 7;
 const chainLength = 2000;
 const allowedChars = 36;
@@ -18,16 +17,16 @@ const pad = (string, chunkSize = passwordLength, char = '0') => {
 };
 assert(pad('1') === '0000001');
 
-const chains = Array(chainLength).fill().map((value, index) => pad(index.toString(allowedChars)));
+const chains = Array(13 /* chainLength*/).fill().map((value, index) => pad(index.toString(allowedChars)));
 assert(chains[0] === '0000000');
-assert(chains[12] === '000000c');
+// assert(chains[12] === '000000c');
 
 const reductionFunction = (hashString, step) => {
   const hashAsNumber = new BigNumber(hashString, 16);
   let stepHash = hashAsNumber.plus(step);
   let reducedPassword = '';
 
-  initialPassword.split('').forEach(() => {
+  Array(passwordLength).fill().forEach(() => {
     reducedPassword += stepHash.modulo(allowedChars).toString(36);
     stepHash = stepHash.dividedToIntegerBy(allowedChars);
   });
@@ -36,19 +35,28 @@ const reductionFunction = (hashString, step) => {
 };
 assert(reductionFunction('29c3eea3f305d6b823f562ac4be35217', 0) === '87inwgn');
 
-const rainbowChain = [{
-  password: initialPassword,
-  hash: md5(initialPassword),
-}];
-for (let i = 0; i < 2000; i += 1) {
-  rainbowChain.push({
-    password: reductionFunction(rainbowChain[i].hash, i),
-    hash: md5(reductionFunction(rainbowChain[i].hash, i)),
-  });
-}
+const rainbowChain = chains.map((password) => {
+  console.log('calculating row', password);
 
-assert(rainbowChain[1].password === '87inwgn');
-assert(rainbowChain[1].hash === '12e2feb5a0feccf82a8d4172a3bd51c3');
-assert(rainbowChain[2].password === 'frrkiis');
-assert(rainbowChain[2].hash === '437988e45a53c01e54d21e5dc4ae658a');
+  const row = [{
+    password,
+    hash: md5(password),
+  }];
+
+  for (let i = 0; i < 2000; i += 1) {
+    row.push({
+      password: reductionFunction(row[i].hash, i),
+      hash: md5(reductionFunction(row[i].hash, i)),
+    });
+  }
+
+  return row;
+});
+
+console.log(rainbowChain);
+
+assert(rainbowChain[0][1].password === '87inwgn');
+assert(rainbowChain[0][1].hash === '12e2feb5a0feccf82a8d4172a3bd51c3');
+assert(rainbowChain[0][2].password === 'frrkiis');
+assert(rainbowChain[0][2].hash === '437988e45a53c01e54d21e5dc4ae658a');
 
