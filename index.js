2b017bc2de557c13309197bd5aca5cc37ecef8ee
Simon Emanuel Schmid <simon@schmid.io>
2017-05-16 18:04:42 +0200

    build rainbow chain

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ assert(md5('0000000') === '29c3eea3f305d6b823f562ac4be35217');
 
 const initialPassword = '0000000';
 const passwordLength = 7;
+const chainLength = 2000;
 
 const allowedChars = [
   '0', '1', '2', '3', '4', '5',
@@ -23,11 +24,28 @@ const reductionFunction = (hashString, step) => {
   let stepHash = hashAsNumber.plus(step);
   let reducedPassword = '';
 
-  initialPassword.split().forEach(() => {
+  initialPassword.split('').forEach(() => {
     reducedPassword += allowedChars[stepHash.modulo(allowedChars.length)];
     stepHash = stepHash.dividedToIntegerBy(allowedChars.length);
   });
 
-  return stepHash;
+  return reducedPassword.split('').reverse().join('');
 };
-assert(reductionFunction('29c3eea3f305d6b823f562ac4be35217', 1), '87inwgn');
+assert(reductionFunction('29c3eea3f305d6b823f562ac4be35217', 0) === '87inwgn');
+
+const rainbowChain = [{
+  password: initialPassword,
+  hash: md5(initialPassword),
+}];
+for (let i = 0; i < 2000; i += 1) {
+  rainbowChain.push({
+    password: reductionFunction(rainbowChain[i].hash, i),
+    hash: md5(reductionFunction(rainbowChain[i].hash, i)),
+  });
+}
+
+assert(rainbowChain[1].password === '87inwgn');
+assert(rainbowChain[1].hash === '12e2feb5a0feccf82a8d4172a3bd51c3');
+assert(rainbowChain[2].password === 'frrkiis');
+assert(rainbowChain[2].hash === '437988e45a53c01e54d21e5dc4ae658a');
+
